68bd14c4141df5fd75941997acc306f246ca544c
Victor Dionizio <victor.dionizio28@gmail.com>
2022-12-15 18:29:11 -0300

    Resolvendo conflitos

--- app/src/main/java/dionizio/victor/crud/ui/login/view/LoginActivity.java
+++ app/src/main/java/dionizio/victor/crud/ui/login/view/LoginActivity.java
@@ -1,7 +1,16 @@
 package dionizio.victor.crud.ui.login.view;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import dionizio.victor.crud.R;
 
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
