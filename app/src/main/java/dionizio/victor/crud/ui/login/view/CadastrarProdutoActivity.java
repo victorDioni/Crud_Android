e3e3ba50d3e9c5f8836c54d84b512fd47315d9e8
Victor Dionizio <victor.dionizio28@gmail.com>
2023-01-05 21:12:23 -0300

    Criando a tela de cadastrar produto

---
+++ app/src/main/java/dionizio/victor/crud/ui/login/view/CadastrarProdutoActivity.java
@@ -0,0 +1,16 @@
+package dionizio.victor.crud.ui.login.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import dionizio.victor.crud.R;
+
+public class CadastrarProdutoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_produto);
+    }
+}
--- app/src/main/java/dionizio/victor/crud/ui/login/view/LoginActivity.java
+++ app/src/main/java/dionizio/victor/crud/ui/login/view/LoginActivity.java
@@ -1,6 +1,9 @@
 package dionizio.victor.crud.ui.login.view;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +11,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import dionizio.victor.crud.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnLogin;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+//        btnLogin = findViewById(R.id.btnEntrarLogin);
+//
+//        btnLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                cadastrarProduto();
+//            }
+//        });
+    }
+
+    public void cadastrarProduto(){
+        startActivity(new Intent(this, CadastrarProdutoActivity.class));
     }
 }
